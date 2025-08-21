e3ba7fcda9d1e9fb9e71f497bfce2f7fd62eb180
HebertyRichards <heberynho666@hotmail.com>
2025-08-21 18:37:20 -0300

    fix: fix rules page

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -86,62 +86,62 @@ export default function Rules() {
             className="list-decimal list-inside space-y-4 text-gray-700 dark:text-gray-300 text-justify 
                marker:font-bold marker:text-gray-700 marker:dark:text-white"
           >
-            <li>Prezamos pelo mínimo de lealdade.</li>
+            <li>{`Prezamos pelo mínimo de lealdade.`}</li>
             <li>
-              Não fale sobre os assuntos da Auditore com gente de fora. Meio
+              {`Não fale sobre os assuntos da Auditore com gente de fora. Meio
               óbvio, né? Não divulgue o que acontece internamente com gente de
-              fora.
+              fora.`}
             </li>
             <li>
-              Evite brigas internas. Caso algo te incomode, procure um dos
-              responsáveis da Auditore pra resolver no privado.
+              {`Evite brigas internas. Caso algo te incomode, procure um dos
+              responsáveis da Auditore pra resolver no privado.`}
             </li>
             <li>
-              O que acontece no nosso grupo do WhatsApp e Discord, fica no nosso
+              {`O que acontece no nosso grupo do WhatsApp e Discord, fica no nosso
               grupo. Não fique espalhando as coisas que falamos e compartilhamos
               por aí (isso vale pra tudo, figurinhas, links e outras coisas
-              menos importantes).
+              menos importantes).`}
             </li>
             <li>
-              Não panelar errado. Usamos bastante esse termo pra quando alguém
+              {`Não panelar errado. Usamos bastante esse termo pra quando alguém
               está prestes a fazer ou falar uma besteira grande. Se você sabe
               que fazer algo vai prejudicá-lo(a), não faça. Depois não adianta
               ficar chorando porque foi banido e querendo pagar de certo. Se
-              você fez besteira, assuma seu B.O e pronto, vida que segue.
+              você fez besteira, assuma seu B.O e pronto, vida que segue.`}
             </li>
             <li>
-              Não permitimos que a Auditore (SA:MP) tenha 2 líderes de
+              {`Não permitimos que a Auditore (SA:MP) tenha 2 líderes de
               organizações ao mesmo tempo; apenas um por vez é permitido.
               Lembre-se: sempre avise antes de assumir a liderança, pois aqui
-              não é bagunça.
+              não é bagunça.`}
             </li>
             <li>
-              É proibido qualquer ato que prejudique a Auditore e seus membros.
+              {`É proibido qualquer ato que prejudique a Auditore e seus membros.
               Exemplos: conspirar contra e deixar o grupo em segundo plano. Se
               você entrou no jogo e viu que a Auditore está de Grove, cole de
               Grove. Porra, a gente dando a vida na guerra de Grove e você cola
               de Vagos? E o bom senso? Isso é atitude de gente desunida. Não
-              quero esse tipo de coisa acontecendo por aqui.
+              quero esse tipo de coisa acontecendo por aqui.`}
             </li>
             <li>
-              Todos têm o direito de expressar suas opiniões, desde que de forma
+              {`Todos têm o direito de expressar suas opiniões, desde que de forma
               respeitosa, sem rebaixar ou humilhar ninguém. Sugestões são sempre
-              bem-vindas.
+              bem-vindas.`}
             </li>
             <li>
-              Recrutamos apenas pessoas que acreditamos ter o perfil da
+              {`Recrutamos apenas pessoas que acreditamos ter o perfil da
               Auditore; colar junto não é suficiente. Essa análise será feita
-              pelos responsáveis.
+              pelos responsáveis.`}
             </li>
             <li>
-              Se por algum motivo você deseja sair da Auditore, procure o
-              responsável no privado e fale a verdade. Diga: "Olha, eu estou
-              saindo da Auditore por esse motivo" ou simplesmente nem precisa
+              {`Se por algum motivo você deseja sair da Auditore, procure o
+              responsável no privado e fale a verdade. Diga: 'Olha, eu estou
+              saindo da Auditore por esse motivo' ou simplesmente nem precisa
               falar o motivo, só diga: "Estou saindo da família por motivos
               pessoais, obrigado pela oportunidade". Só não fique inventando
               desculpinhas pra sair, porque isso fica muito na cara e vai ficar
               feio pra você. Se for sair, saia de cabeça erguida, sem mentiras
-              ou tretas com ninguém.
+              ou tretas com ninguém.`}
             </li>
           </ol>
         </div>
